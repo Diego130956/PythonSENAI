453aaa406b96e02afb42c7d63a304c7e4481f265
Diego130956 <diegobertaglia450@gmail.com>
2025-05-19 10:51:45 -0300

    Add files via upload

---
+++ Situações problemas/Exercico Área do hexagono.py
@@ -0,0 +1,10 @@
+l = int(input('Lado do triangulo'))
+l2 = l * l
+raiz = round(3 ** 0.5)
+l4 = l2 * raiz
+at = l4 / 4
+l3 = print('A area do triangulo e', at)
+print('Agora vamos para a area do hexagono')
+h = 6
+print('a area do hexagono e', at * h)
+
